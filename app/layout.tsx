295f87738a0e3addf23121bb5f984d859755bbe9
helen-akateva <akateva17@gmail.com>
2025-10-21 16:55:50 +0300

    add header foote rpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import Home from "./page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+          <Header />
+       <main> {children} </main> 
+
+        <Footer/>
       </body>
     </html>
   );
